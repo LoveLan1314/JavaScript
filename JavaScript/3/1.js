affb9a5e86a97bc83cda071b8fbfb9078a20e193
LoveLan1314 <736265181@qq.com>
2018-03-13 00:30:04 +0800

    3.4.6

---
+++ JavaScript/3/1.js
@@ -0,0 +1,23 @@
+﻿"use strict";
+function doSomething() {
+    "use strict";
+    //函数体
+}
+
+//var message;
+
+//var message = "hi";
+//var message = "hi",
+//    fount = false,
+//    age = 29;
+
+//var message = "some string";
+//alert(typeof message);
+//alert(typeof (message));
+//alert(typeof 95);
+
+//var message;
+//alert(message == undefined);
+
+//var result = Number.MAX_VALUE + Number.MAX_VALUE;
+//alert(isFinite(result));
